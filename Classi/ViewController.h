70eb4bd14e3788024c12d134f0d64873a6e7faaf
Claudio Franzi <claudio@MbProClaudio.local>
2014-10-05 16:42:42 +0200

    Initial Commit

---
+++ Classi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Classi
+//
+//  Created by Claudio Franzi on 05/10/14.
+//  Copyright (c) 2014 ITIS Barsanti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Classi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Classi
+//
+//  Created by Claudio Franzi on 05/10/14.
+//  Copyright (c) 2014 ITIS Barsanti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
